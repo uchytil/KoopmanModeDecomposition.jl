f6ea8bb98e780a18c9943a74dee755498a2cac29
Documenter.jl <documenter@juliadocs.github.io>
2026-04-06 21:07:56 +0000

    build based on 0fdf916

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"#KoopmanModeDecomposition","page":"Home","text":"Documentation for KoopmanModeDecomposition.\n\n","title":"KoopmanModeDecomposition"}]
+}
